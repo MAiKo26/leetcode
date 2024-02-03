f42ee9c2c03d93c7c059dba01ee29e4abee383f7
Mohamed Aziz Karoui <133112004+MAiKo26@users.noreply.github.com>
2024-02-04 00:09:50 +0100

    251 ms / 60.2 MB

---
+++ 2. Palindrome Number/Solution.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    y = x + " ";
+    console.log("First",y);
+    for (var i = 0 ; i < y.length -1 ; i++)
+        {
+            if (y[i] != y[(y.length -2 -i )])
+                return false;
+        }
+    return true;
+};
