a269506a402693d24f8b6a5a3c77be84e71ce64b
Mohamed Aziz Karoui <133112004+MAiKo26@users.noreply.github.com>
2024-02-03 23:03:40 +0100

    345 ms / 42.5 mb

---
+++ 1. Two Sum /Solution.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    var i = 0;
+    var j = 0 ;
+    var x = false ;
+    var nums2 = [] ;
+    
+    while (x=== false){
+        if (i > nums.length )
+            x = true;
+        if (j > nums.length)
+           { i++; j= 0 ; }
+        if ((nums[i] + nums[j] == target) && j != i)
+           { nums2[0] = i;
+            nums2[1] = j ;
+            x = true ;
+    }
+        
+        j++ ;
+        }
+        return nums2 ;
+};
