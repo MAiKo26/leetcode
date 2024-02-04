da2e91142b60f50e615d36c8c95c3f1f68a90f07
Mohamed Aziz Karoui <133112004+MAiKo26@users.noreply.github.com>
2024-02-04 11:18:37 +0100

    56 ms / 49.3 MB

---
+++ 66. Plus One/Solution.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    var l = digits.length -1 ;
+    while (l > -1){
+        if (digits[l] != 9){
+            digits[l] = digits[l] +1;
+            return digits;
+        }
+
+
+        digits[l] = 0 ;
+        
+                if (l == 0){
+            digits.unshift(1);
+            return digits;
+                 }
+        
+        
+        l--;
+        
+        
+    }
+};
