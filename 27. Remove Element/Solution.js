e497211cd1a9e414538399bb348bcceaa15fd099
Mohamed Aziz Karoui <133112004+MAiKo26@users.noreply.github.com>
2024-02-04 11:12:49 +0100

    49 ms / 49.1 MB

---
+++ 27. Remove Element/Solution.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let k=0;
+    
+    for(let i = 0; i < nums.length; i++){
+        if (nums[i] == val) {nums[i] = -1;k++}
+    }
+    
+    nums.sort((a, b) => (a > b ? -1 : 0));
+    
+    return (nums.length - k);
+};
