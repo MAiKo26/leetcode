8dd133ced7c7d2d26d9d7947dd3bf7c3e322a9c2
Mohamed Aziz Karoui <133112004+MAiKo26@users.noreply.github.com>
2024-02-04 11:15:19 +0100

    46 ms / 49.2 MB

---
+++ 35. Search Insert Position/Solution.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    return (nums.findIndex((element) => element >= target) == -1) ? nums.length :  nums.findIndex((element) => element >= target)
+    
+};
