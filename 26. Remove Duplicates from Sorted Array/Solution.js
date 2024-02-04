b7d84fd75721952cc38b4682837e3e4c38ff35a6
Mohamed Aziz Karoui <133112004+MAiKo26@users.noreply.github.com>
2024-02-04 11:12:04 +0100

    896 ms / 43.8 MB

---
+++ 26. Remove Duplicates from Sorted Array/Solution.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    var k = nums.length ;
+    for (var i = 0 ; i < nums.length ; i++){
+        if (nums[i] == nums[i+1] && nums[i+1] != null){
+            for (var j = i ; j < nums.length ; j++){
+                nums[j]= nums[j+1] ;
+                
+            }
+           k-- ;
+           i--;
+        }
+    }
+
+    return k ;
+};
