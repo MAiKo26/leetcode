5ec10baf4ec3399c332b73490b52171d7a368856
Mohamed Aziz Karoui <133112004+MAiKo26@users.noreply.github.com>
2024-02-09 09:34:29 +0100

    51 ms / 48.9 MB

---
+++ 00119. Pascal's Triangle II/Solution.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+var getRow = function(rowIndex) {
+    return generate(rowIndex+1)[rowIndex];
+};
+
+var generate = function(numRows) {
+    let Rows = [];
+    for(let i=1;i < numRows+1;i++){
+        let Row = [];
+        for (let j=0;j < i;j++){
+            Row.push(1);
+        }
+        Rows.push(Row);
+    }
+
+    for(let i=2;i < numRows;i++){
+        for(let j=1; j<i ;j++){
+            Rows[i][j] = Rows[i-1][j-1] + Rows[i-1][j] ;
+            
+        }
+    }
+    
+    return Rows;
+};
